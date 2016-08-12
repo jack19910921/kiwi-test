21fd562a15a04f4a28ea26dfb3b9ab8b5b7eb7c5
jack19910921 <870313180@qq.com>
2016-08-13 07:44:05 +0800

    Initial test sub project

---
+++ src/main/java/com/vip/study/test/BaseTest.java
@@ -0,0 +1,22 @@
+package com.vip.study.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+
+/**
+ * Created by jack08.liu on 2016/8/11.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-test.xml"}, inheritLocations = true)
+@TestExecutionListeners({ServletTestExecutionListener.class, DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class, InitSpringProfilesExecutionListenr.class})
+@ActiveProfiles(value = "test")
+public abstract class BaseTest {
+
+}
---
+++ src/main/java/com/vip/study/test/InitSpringProfilesExecutionListenr.java
@@ -0,0 +1,36 @@
+package com.vip.study.test;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+/**
+ * Created by jack08.liu on 2016/8/11.
+ */
+public class InitSpringProfilesExecutionListenr extends AbstractTestExecutionListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(InitSpringProfilesExecutionListenr.class);
+
+    private static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+
+    @Override
+    public void beforeTestClass(TestContext testContext) throws Exception {
+        try {
+            Class<?> testClass = testContext.getTestClass();
+            ActiveProfiles activeProfiles = testClass.getAnnotation(ActiveProfiles.class);
+            if (activeProfiles != null) {
+                String[] value = activeProfiles.value();
+                System.setProperty(SPRING_PROFILES_ACTIVE, value[0]);
+            }
+            LOGGER.info("beforeTestClass called with [" + testContext + "].");
+        } catch (RuntimeException e) {
+            LOGGER.info("init [" + this.getClass().getName() + "] has some problem:[" +
+                    StringUtils.left(e.getMessage(), 500) + "].");
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(e.getMessage(), e);
+            }
+        }
+    }
+}
